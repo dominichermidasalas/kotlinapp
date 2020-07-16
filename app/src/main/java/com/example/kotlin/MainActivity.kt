d3e2ade496df54fe5c3a7d7c9b375dad3552fc88
Dominic Hermida Salas <dominic.hermida@kueski.com>
2020-07-16 12:00:45 -0500

    Added nav graph, up button, menu

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.myNavHostFragment).navigateUp()
+                || super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/com/example/kotlin/RegisterFragment.kt
+++ app/src/main/java/com/example/kotlin/RegisterFragment.kt
@@ -1,11 +1,12 @@
 package com.example.kotlin
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.kotlin.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
@@ -16,6 +17,22 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentRegisterBinding>(
             inflater, R.layout.fragment_register, container, false)
+
+        binding.continuePhoneButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_registerFragment_to_confirmFragment)
+        )
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
